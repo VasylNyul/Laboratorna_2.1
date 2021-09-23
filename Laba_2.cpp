ee2725a825d26d0e6a1622a9daf58bcbcf19aeec
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-09-23 14:14:39 +0300

    test11

--- Laba_2.cpp
+++ Laba_2.cpp
@@ -14,16 +14,16 @@
             double Pi = 4 * atan(1.); // число пі
             double a;  // вхідний параметр
             double z1; // результат обчислення 1-го виразу
-            double z2; // результат обчислення 2-го виразу
+            //double z2; // результат обчислення 2-го виразу
      
             cout << "a = "; cin >> a;
      
             z1 = cos(3./8*Pi-a/4)*cos(3./8*Pi-a/4)-cos(11./8*Pi+a/4)*cos(11./8*Pi+a/4);
-            z2 = sqrt(2)/2*sin(a/2);
+            //z2 = sqrt(2)/2*sin(a/2);
      
             cout << endl;
      cout << "z1 = " << z1 << endl;
-     cout << "z2 = " << z2 << endl;
+     //cout << "z2 = " << z2 << endl;
      cin.get();
      return 0;
 }
